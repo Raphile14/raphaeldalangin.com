3c0629a4b00ad439e290c791520b4da2ed55358a
Raphile14 <rllddalangin@gmail.com>
2022-01-30 10:14:36 +0800

    Basic Page Not Found Layout

--- src/components/pages/misc/PageNotFound.jsx
+++ src/components/pages/misc/PageNotFound.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function PageNotFound() {
     document.title = "Page Not Found";
-  return <div> Page Not Found </div>;
+    return (
+        <div id="wrapper">
+            <div id="main">
+                <div className='nf__center'>
+                    <h1>
+                        PAGE NOT FOUND!
+                    </h1>
+                    <p> Woops! How did you get here? Click <Link to="/"> here</Link> to go back to my portfolio! Take care!</p>
+                </div>
+            </div>
+        </div>
+    );
 }
