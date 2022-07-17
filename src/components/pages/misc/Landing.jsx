68a7b782b5687457c454131550ab49fb4d2a572f
Raphile14 <rllddalangin@gmail.com>
2022-07-17 22:14:51 +0800

    Improved assets and updated info

--- src/components/pages/misc/Landing.jsx
+++ src/components/pages/misc/Landing.jsx
@@ -7,19 +7,27 @@ export default function landing() {
     document.title = "Raphael Dalangin's Portfolio";
     return (
         // < !--Wrapper -- >
-        <div id="wrapper" className="fade-in">
+        <div id='wrapper' className='fade-in'>
             {/* <!-- Main --> */}
-            <div id="main">
-
+            <div id='main'>
                 {/* Main Description, Image, and Social Media */}
                 <div className='main__header justify-content-center'>
-                    <img className='main__img' src="images/Raphael Dalangin.jpg" alt="me" />
+                    <img
+                        className='main__img'
+                        src='images/Raphael Dalangin.jpg'
+                        alt='me'
+                    />
                     <br />
                     <p className='main__center'>
-                        Hi, I am <i><b>Raphael Dalangin</b></i>, an aspiring Software, Game, and Web
-                        Developer from the Philippines. Currently a 4th Year College Student at Malayan Colleges
-                        Mindanao. Taking up Bachelor of Science in Computer Science. I love playing video games,
-                        playing the guitar, and composing music! Contact me <a href='#contacts'> here </a>
+                        Hi, I am{' '}
+                        <i>
+                            <b>Raphael Dalangin</b>
+                        </i>
+                        , an aspiring Software, Game, and Web Developer from the
+                        Philippines. A Bachelor of Science in Computer Science
+                        graduate from Mapua Malayan Colleges Mindanao. I love
+                        playing video games, playing the guitar, and composing
+                        music! Contact me <a href='#contacts'> here </a>
                     </p>
                     <br />
 
@@ -36,14 +44,21 @@ export default function landing() {
             </div>
 
             {/* <!-- Contacts --> */}
-            <div id="contacts" className='text-center mt-5'>
+            <div id='contacts' className='text-center mt-5'>
                 <h2 className='mb-4 card__title'> Contact Details </h2>
                 <h5>
-                    <a className='contacts__link' href="mailto: raphaeldalangin@codingcatharsis.com" target="_blank" rel="noreferrer">email: raphaeldalangin@codingcatharsis.com</a>
+                    <a
+                        className='contacts__link'
+                        href='mailto: rl.dalangin@works-telemondo.com'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        email: rl.dalangin@works-telemondo.com
+                    </a>
                 </h5>
-                <h5>
+                {/* <h5>
                     <a className='contacts__link' href="/assets/files/v2 Raphael Dalangin - Resume.pdf" target="_blank" rel="noreferrer"> file: resume </a>
-                </h5>
+                </h5> */}
 
                 <SocMedCard />
                 <br />
